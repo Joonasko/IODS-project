dfd11594e8f6348affbea980ed8bf714b00f0cdf
Kolstela Joonas (FMI) <joonas.kolstela@fmi.fi>
2022-11-17 18:55:48 +0200

    Created the create_alc file and added exercise3

---
+++ data/create_alc.R
@@ -0,0 +1,3 @@
+# Joonas Kolstela, 17.11.2022 18:53, script for datawrangling and analysis of the UCI machine learning student performance data.
+# Data source: https://archive.ics.uci.edu/ml/datasets/Student+Performance
+
